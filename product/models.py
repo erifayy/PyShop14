5cf5b3e5714eb4fb548981431c109fed9fd684d5
aiperi <aiperibzkva@gmail.com>
2021-11-03 16:19:59 +0600

    CRUD, CBV

--- product/models.py
+++ product/models.py
@@ -8,7 +8,9 @@ from django.db.models.enums import Choices
 #TODO: Notebooks --> ACER, MacBook, ASUS
 #TODO: Accessories --> Earphones, PowerBank
 #TODO: по 2 продукта на каждую категорию
- 
+
+
+
 
 class Category(models.Model):
     title = models.CharField(max_length=50, unique=True)
@@ -17,7 +19,7 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         if self.parent:
-            return f'{self.parent} --> {self.title}'
+            return f"{self.parent} --> {self.title}"
         return self.title
 
 
@@ -33,7 +35,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='prod_images')
-    catagory = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.name
--- product/views.py
+++ product/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .forms import CreateProductForm, UpdateProductForm
+from .models import Product
+from .models import Category
+
+
+def home_page(request):
+    categories = Category.objects.all()
+    print(categories)
+    return render(request, 'home.html', locals())
+
+
+def product_list(request, slug):
+    products = Product.objects.filter(category__slug=slug)
+    # SELECT * FROM Product WHERE PRODUCT.SLUG = SLUG
+    return render(request, 'product_list.html', locals())
+
+
+def product_detail(request, product_id):
+    # product = Product.object.get(id=product_id)
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'detail.html', locals())
+
+
+def product_create(request):
+    if request.method == 'POST':
+        print(request.POST)
+        product_form = CreateProductForm(request.POST,
+                                         request.FILES)
+        if product_form.is_valid():
+            product = product_form.save()
+            return redirect('detail', product.id)
+    else:
+        product_form = CreateProductForm()
+    return render(request, 'create_product.html', locals())
+
+
+def product_update(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product_form = UpdateProductForm(request.POST or None, request.FILES or None, instance=product)
+    if product_form.is_valid():
+        product_form.save()
+        return redirect('detail', product_id)
+
+    return render(request, 'update_product.html', locals())
+
+
+def product_delete(request, product_id, ):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        product.delete()
+        slug = product.category.slug
+        return redirect('list', slug)
+    return render(request, 'delete_product.html', locals())
